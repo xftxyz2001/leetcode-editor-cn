d0b11508c667d828a1095c6baa330716d1e6274b
通晓宇宙 <2581011320@qq.com>
2023-09-21 21:11:31 +0800

    2023年9月21日

---
+++ [2553]分割数组中数字的数位.java
@@ -0,0 +1,97 @@
+//给你一个正整数数组 nums ，请你返回一个数组 answer ，你需要将 nums 中每个整数进行数位分割后，按照 nums 中出现的 相同顺序 放入答案
+//数组中。 
+//
+// 对一个整数进行数位分割，指的是将整数各个数位按原本出现的顺序排列成数组。 
+//
+// 
+// 比方说，整数 10921 ，分割它的各个数位得到 [1,0,9,2,1] 。 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：nums = [13,25,83,77]
+//输出：[1,3,2,5,8,3,7,7]
+//解释：
+//- 分割 13 得到 [1,3] 。
+//- 分割 25 得到 [2,5] 。
+//- 分割 83 得到 [8,3] 。
+//- 分割 77 得到 [7,7] 。
+//answer = [1,3,2,5,8,3,7,7] 。answer 中的数字分割结果按照原数字在数组中的相同顺序排列。
+// 
+//
+// 示例 2： 
+//
+// 输入：nums = [7,1,3,9]
+//输出：[7,1,3,9]
+//解释：nums 中每个整数的分割是它自己。
+//answer = [7,1,3,9] 。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 1000 
+// 1 <= nums[i] <= 10⁵ 
+// 
+//
+// Related Topics 数组 模拟 👍 10 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+class Solution {
+    public int[] separateDigits(int[] nums) {
+        StringBuilder sb = new StringBuilder();
+        for (int i : nums) {
+            sb.append(i);
+        }
+        int[] ans = new int[sb.length()];
+        for (int i = 0, n = sb.length(); i < n; i++) {
+            ans[i] = sb.charAt(i) - '0';
+        }
+        return ans;
+    }
+    // public int[] separateDigits(int[] nums) {
+    //     ArrayList<Integer> ans = new ArrayList<>();
+    //     for (int n : nums) {
+    //         if (n < 10) {
+    //             ans.add(n);
+    //         } else if (n < 100) {
+    //             ans.add(n / 10);
+    //             ans.add(n % 10);
+    //         } else if (n < 1000) {
+    //             ans.add(n / 100);
+    //             ans.add(n / 10 % 10);
+    //             ans.add(n % 10);
+    //         } else if (n < 10000) {
+    //             ans.add(n / 1000);
+    //             ans.add(n / 100 % 10);
+    //             ans.add(n / 10 % 10);
+    //             ans.add(n % 10);
+    //         } else if (n < 100000) {
+    //             ans.add(n / 10000);
+    //             ans.add(n / 1000 % 10);
+    //             ans.add(n / 100 % 10);
+    //             ans.add(n / 10 % 10);
+    //             ans.add(n % 10);
+    //         } else if (n < 1000000) {
+    //             ans.add(n / 100000);
+    //             ans.add(n / 10000 % 10);
+    //             ans.add(n / 1000 % 10);
+    //             ans.add(n / 100 % 10);
+    //             ans.add(n / 10 % 10);
+    //             ans.add(n % 10);
+    //         }
+    //     }
+    //     int[] ansArray = new int[ans.size()];
+    //     for (int i = 0; i < ans.size(); i++) {
+    //         ansArray[i] = ans.get(i);
+    //     }
+    //     return ansArray;
+    // }
+}
+//leetcode submit region end(Prohibit modification and deletion)
